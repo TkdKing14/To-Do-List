cedb6d3c898e61c754dd195e8e34e8f46a206bc2
Carson Payne <carson.j.payne14@gmail.com>
2025-01-08 11:57:41 -0600

    Initial Commit

---
+++ To Do List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To Do List
+//
+//  Created by Carson Payne on 1/8/25.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
