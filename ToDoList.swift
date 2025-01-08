83ce80b1e86bbbe0268275139ac66670135b933c
Carson Payne <carson.j.payne14@gmail.com>
2025-01-08 12:30:43 -0600

    Created custom ToDoList class

---
+++ ToDoList.swift
@@ -0,0 +1,13 @@
+//
+//  ToDoList.swift
+//  To Do List
+//
+//  Created by Carson Payne on 1/8/25.
+//
+
+import Foundation
+class ToDoList: ObservableObject {
+    @Published var items = [ToDoItem(priority: "High", description: "Take out trash", dueDate: Date()),
+                            ToDoItem(priority: "Medium", description: "Pick up clothes", dueDate: Date()),
+                            ToDoItem(priority: "Low", description: "Eat a donut", dueDate: Date())]
+ }
